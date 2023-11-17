004e60fe4a552d6e9ef50d613d0a7cbe6e1dd741
minwoogramer <01082249606nmw@gmail.com>
2023-11-17 15:04:56 +0900

    ValueTypes.sol + NFT

---
+++ Solidity/basic/ValueTypes.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract ValueTypes {
+    // Write your code here
+    bool public b = true;
+    int public i;
+    int public u = 123;
+    address public addr = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    bytes32 public b32 =
+        0x89c58ced8a9078bdef2bb60f22e58eeff7dbfed6c2dff3e7c508b629295926fa;
+    bytes32 public myBytes32 =
+        0x941e989004faa89f57a42219b3bff64ab892125072da21345be2b7c7d3ca9ddc;
+}
